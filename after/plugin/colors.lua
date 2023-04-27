c74fbb4c9bd9c89e5c7a835442bc9e32c8247523
Tim Vick <t.vick@yahoo.com>
2023-04-26 19:22:29 -0700

    chore: change default colorscheme (#18)

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,4 @@
-vim.cmd.colorscheme("sonokai")
+vim.cmd.colorscheme("melange")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, 'LineNr', { fg = "#c2c7d1" } )
