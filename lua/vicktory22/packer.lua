4f2da7a0a85fe0815216c22189af3b87aa463f1e
Tim Vick <t.vick@yahoo.com>
2023-05-25 10:00:02 -0700

    feat: remove dap (#21)

--- lua/vicktory22/packer.lua
+++ lua/vicktory22/packer.lua
@@ -48,14 +48,6 @@ return require('packer').startup(function(use)
         }
     }
 
-    use('mfussenegger/nvim-dap')
-    use { 'mxsdev/nvim-dap-vscode-js', requires = {'mfussenegger/nvim-dap'} }
-    use {
-      'microsoft/vscode-js-debug',
-      opt = true,
-      run = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out'
-    }
-
     use('github/copilot.vim')
 
     use({
