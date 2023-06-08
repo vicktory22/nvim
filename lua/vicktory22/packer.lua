e9842342207bef9bbb5d31979c26500fef1db3d0
Tim Vick <t.vick@yahoo.com>
2023-06-08 11:38:41 -0700

    feat: add vim-fugitive and vim-rhubarb

--- lua/vicktory22/packer.lua
+++ lua/vicktory22/packer.lua
@@ -23,6 +23,8 @@ return require('packer').startup(function(use)
     use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use('tpope/vim-rhubarb')
     use('lewis6991/gitsigns.nvim')
 
     use {
