143207ab23f4e9c1074f5f4ead95d34bf66efad6
Tim Vick <t.vick@yahoo.com>
2023-06-09 08:57:39 -0700

    chore: update default color to rose pine moon (#26)

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,4 @@
-vim.cmd.colorscheme("nordic")
+vim.cmd.colorscheme("rose-pine-moon")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, 'LineNr', { fg = "#c2c7d1" } )
