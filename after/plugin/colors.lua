b6fbf2483812e307374cd2e15a363c10c820dded
Tim Vick <t.vick@yahoo.com>
2023-11-18 09:29:41 -0800

    chore: remove setup color scheme function

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,14 +2,3 @@ vim.cmd.colorscheme("rose-pine-moon")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, 'LineNr', { fg = "#c2c7d1" } )
-
-function SetupColorScheme(color)
-	color = color or "dracula"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_set_hl(0, 'LineNr', { fg = "#c2c7d1" } )
-end
-
-
